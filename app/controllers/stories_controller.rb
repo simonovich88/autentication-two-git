32e839df663513f8dc2af8a98f4c69767a9ee20c
Simon <acarigua88@mail.com>
2020-09-20 12:29:02 -0300

    autentication_two

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -24,7 +24,7 @@ class StoriesController < ApplicationController
   # POST /stories
   # POST /stories.json
   def create
-    @story = Story.new(story_params)
+    @story = Story.new(story_params.merge(user:current_user))
 
     respond_to do |format|
       if @story.save
@@ -40,16 +40,21 @@ class StoriesController < ApplicationController
   # PATCH/PUT /stories/1
   # PATCH/PUT /stories/1.json
   def update
-    respond_to do |format|
-      if @story.update(story_params)
-        format.html { redirect_to @story, notice: 'Story was successfully updated.' }
-        format.json { render :show, status: :ok, location: @story }
-      else
-        format.html { render :edit }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
+    if current_user.admin? || @story.user.id == current_user.id
+      respond_to do |format|
+        if @story.update(story_params)
+          format.html { redirect_to @story, notice: 'Story was successfully updated.' }
+          format.json { render :show, status: :ok, location: @story }
+        else
+          format.html { render :edit }
+          format.json { render json: @story.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to stories_path, notice: 'Only admins or the user who created the story can update'
     end
   end
+
 
   # DELETE /stories/1
   # DELETE /stories/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers:{registrations: "users/registrations"}
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
